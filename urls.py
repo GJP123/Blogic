f1b6aa17d4efbae2cdd1f58d44c7c38bd72ee726
graham <graham@Q330>
2011-08-15 16:46:12 +0100

    Interim commit

--- urls.py
+++ urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls.defaults import *
+from django.views.generic import DetailView, ListView
+from blog.models import Blog
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
-    ('^$', 'django.views.generic.simple.direct_to_template',
-     {'template': 'home.html'}),
+    ('^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    (r'^/$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    (r'^/', include('blog.urls')),
 )
+
